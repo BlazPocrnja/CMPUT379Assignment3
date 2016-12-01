26686ea14cc58de049b6f0c04228c16d1787c164
Blaz Pocrnja <blaz@ualberta.ca>
2016-12-01 15:53:16 -0700

    Skeleton structure

---
+++ simulator.c
@@ -0,0 +1,27 @@
+#include "simulator.h"
+
+#define HASH_SIZE 1024
+
+struct node{
+	int key;
+	int value;
+	struct node* next;
+}
+
+struct node* hashTable[HASH_SIZE];
+
+void init(int psize, int winsize){
+
+}
+
+void put(unsigned int address, int value){
+
+}
+
+int get(unsigned int address){
+
+}
+
+void done(){
+
+}
---
+++ simulator.h
@@ -0,0 +1,13 @@
+#ifndef simulator_h
+#define simulator_h
+
+//Sorting process
+void process();
+
+//Simulator functions
+void init(int psize, int winsize);
+void put(unisgned int address,int value);
+int get(unsigned int address);
+void done();
+
+#endif
